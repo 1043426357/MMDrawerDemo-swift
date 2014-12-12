9cbf5286779f0624db0361e10643bd65fdfd9d40
yu.xinwen <yu.xinwen@adways.net>
2014-12-12 16:55:36 +0800

    Initial Commit

---
+++ SliderDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SliderDemo
+//
+//  Created by 余新闻 on 14-12-12.
+//  Copyright (c) 2014年 Adways. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
